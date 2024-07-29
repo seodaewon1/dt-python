abe6f9b379701c1810f40b524a4368f99ae0bfcf
seodaewon1 <162085705+seodaewon1@users.noreply.github.com>
2024-07-29 16:11:30 +0900

    Update kfc.py

--- kfc.py
+++ kfc.py
@@ -1,5 +1,4 @@
 from selenium import webdriver
-from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.chrome.options import Options as ChromeOptions
 from webdriver_manager.chrome import ChromeDriverManager
@@ -8,9 +7,9 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from bs4 import BeautifulSoup
 from datetime import datetime
+from selenium.webdriver import ActionChains
 import time
-import json
-
+import pandas as pd
 # 현재 날짜 가져오기
 current_date = datetime.now().strftime("%Y-%m-%d")
 filename = f"kfc/kfc_{current_date}.json"
